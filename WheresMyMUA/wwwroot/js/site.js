0595ceb8cf262e196ef545038604c9dc1b7a5db8
Aziza Hurt <azizamhurt@gmail.com>
2019-06-13 08:10:20 -0500

    added more components

--- WheresMyMUA/wwwroot/js/site.js
+++ WheresMyMUA/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
